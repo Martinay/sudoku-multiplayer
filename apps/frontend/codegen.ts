efc4558549a9d582d9a8300b5dd4384bacdf9acb
martinay <4411119+Martinay@users.noreply.github.com>
2025-04-27 22:11:15 +0200

    use monorepo

--- apps/frontend/codegen.ts
+++ apps/frontend/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
  
 const config: CodegenConfig = {
-  schema: '../server/src/schema/schema.generated.graphqls',
+  schema: '../backend/src/schema/schema.generated.graphqls',
   documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
