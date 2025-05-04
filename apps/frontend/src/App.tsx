bda74d9f66b52398fea25aa5b01d18775a5cbcc0
martinay <4411119+Martinay@users.noreply.github.com>
2025-05-04 22:53:45 +0200

    center all elements

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,19 +2,21 @@ import Home from "./pages/Home";
 import Game from "./pages/Game";
 import { Provider } from "urql";
 import { client } from "./graphql/client";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { Center, ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 export default function App() {
   return (
     <Provider value={client}>
       <ChakraProvider value={defaultSystem}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sudoku/:gameId" element={<Game />} />
-          </Routes>
-        </BrowserRouter>
+        <Center h="100vh" w="100vw">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/sudoku/:gameId" element={<Game />} />
+            </Routes>
+          </BrowserRouter>
+        </Center>
       </ChakraProvider>
     </Provider>
   );
